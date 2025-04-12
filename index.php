f37cdfb78789ccfb11b0fbe58c914437a247cc86
꧁༒•ƬIПKЦ•༒꧂ <98836849+TinkuMajhi@users.noreply.github.com>
2025-04-13 00:21:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once("simple_html_dom.php");
 // Create DOM from URL or file
 $html = file_get_html('https://www.google.com/');
 
-// Find all images
+// Find all images ..............................................
 foreach($html->find('a') as $element)
        echo $element->href . '<br>';
 ?>
